01bd0eceb9fccba5c7524f48e766fd995c22871e
baite218 <baitemir2.0gmail.com>
2022-04-03 18:51:26 +0600

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import telebot
 import logging
@@ -14,20 +15,56 @@ logger.setLevel(logging.DEBUG)
 db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
 
-@bot.message_handler(commands=['start'])
+
+
+
+
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
+
+
+@bot.message_handler(commands=["start"])
 def start(message):
-	id = message.from_user.id
-	username = message.from_user.username
-	bot.reply_to(message, f"hello, {username}")
+    user_id = message.from_user.id
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
 
-	db_object.execute(f"SELECT id FROM users WHERE id = {id}")
-	result = db_object.fetchone()
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
 
-	if not result:
-		db_object.execute("INSERT INFO users(id, username, message) VALUES (%s, %s, %s)", (id, username, 0))
-		db_connection.commit()
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
 
-"""что это?"""
+    update_messages_count(user_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id)
+
+
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+
+
+
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
@@ -35,9 +72,7 @@ def redirect_message():
     bot.process_new_updates([update])
     return "!", 200
 
-
 if __name__ == "__main__":
-	bot.remove_webhook()
-	bot.set_webhook(url=APP_URL)
-	server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
